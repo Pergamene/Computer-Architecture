cd29a67f664d29f6a1d70a3040be76b5a4538085
Pergamene <smartz373@gmail.com>
2020-08-05 09:45:56 -0600

    started filling out cpu

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,63 +3,72 @@
 import sys
 
 class CPU:
-    """Main CPU class."""
+  """Main CPU class."""
 
-    def __init__(self):
-        """Construct a new CPU."""
-        pass
+  def __init__(self):
+    """Construct a new CPU."""
+    self.ram = [0] * 256
+    self.reg = [0] * 256
+    self.pc_reg = [0] * 8
 
-    def load(self):
-        """Load a program into memory."""
+  def load(self):
+    """Load a program into memory."""
+    address = 0
+    # For now, we've just hardcoded a program:
+    program = [
+      # From print8.ls8
+      0b10000010, # LDI R0,8
+      0b00000000,
+      0b00001000,
+      0b01000111, # PRN R0
+      0b00000000,
+      0b00000001, # HLT
+    ]
+    for instruction in program:
+      self.ram[address] = instruction
+      address += 1
 
-        address = 0
+  def alu(self, op, reg_a, reg_b):
+    """ALU operations."""
+    if op == "ADD":
+      self.reg[reg_a] += self.reg[reg_b]
+    #elif op == "SUB": etc
+    else:
+      raise Exception("Unsupported ALU operation")
 
-        # For now, we've just hardcoded a program:
+  def trace(self):
+    """
+    Handy function to print out the CPU state. You might want to call this
+    from run() if you need help debugging.
+    """
+    print(f"TRACE: %02X | %02X %02X %02X |" % (
+      self.pc,
+      #self.fl,
+      #self.ie,
+      self.ram_read(self.pc),
+      self.ram_read(self.pc + 1),
+      self.ram_read(self.pc + 2)
+    ), end='')
+    for i in range(8):
+      print(" %02X" % self.reg[i], end='')
+    print()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+  def run(self):
+    """Run the CPU."""
+    ir = self.ram_read(self.pc)
+    operand_a = self.ram_read(self.pc + 1)
+    operand_b = self.ram_read(self.pc + 2)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+  def ram_read(self, mar):
+    """
+    Accept the address to read and return the value stored there.
+    MAR == Memory Address Register - address that is being read or written to.
+    """
+    pass
 
-
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
-
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
-        else:
-            raise Exception("Unsupported ALU operation")
-
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
-
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
-
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
-
-        print()
-
-    def run(self):
-        """Run the CPU."""
-        pass
+  def ram_write(self, mdr, mar):
+    """
+    Accept the value to write, and the address to write it to.
+    MDR == Memory Data Register - data that was read or the data to write.
+    """
+    pass
